9d0419a57610882047a554d45891bbf71711bcf8
juansmolano <juansmolano@gmail.com>
2018-10-18 16:27:57 -0500

    first commit

---
+++ deployment/docker/build-docker-hub.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DOCKERHUB_IMAGE=salesgateway
+export DOCKERHUB_TAG=1.0.0
+
+rm -rf deployment/docker/sales-gateway/
+cp -R $API_SHELL_PATH/sales-gateway deployment/docker/sales-gateway
+
+docker build  -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest deployment/docker/
+docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
+docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG
+docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest
