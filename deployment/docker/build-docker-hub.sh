9e1ef683096d91267f37aa32d38e6a654e81f2d7
juansmolano <juansmolano@gmail.com>
2018-10-18 18:15:12 -0500

    changed node image

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=salesgateway
-export DOCKERHUB_TAG=1.0.0
+export DOCKERHUB_TAG=1.0.1
 
 rm -rf deployment/docker/sales-gateway/
 cp -R $API_SHELL_PATH/sales-gateway deployment/docker/sales-gateway
