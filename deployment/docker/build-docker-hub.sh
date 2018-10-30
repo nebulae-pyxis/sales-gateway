468b88c1e88528e523a3692cf8cbe955c79178bf
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-10-30 08:09:14 -0500

    Test gateway

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -5,6 +5,9 @@ export DOCKERHUB_TAG=1.0.4
 rm -rf deployment/docker/sales-gateway/
 cp -R $API_SHELL_PATH/sales-gateway deployment/docker/sales-gateway
 
+ls deployment/docker/
+echo "List files"
+ls deployment/docker/sales-gateway
 docker build  -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest deployment/docker/
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
 docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG
