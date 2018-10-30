31b1603dc6978f0bc87f151c6edc492bef6be4ef
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-10-30 08:24:56 -0500

    Test gateway

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -6,8 +6,10 @@ rm -rf deployment/docker/sales-gateway/
 cp -R $API_SHELL_PATH/sales-gateway deployment/docker/sales-gateway
 
 ls deployment/docker/
-echo "List files"
+echo "- List files"
 ls deployment/docker/sales-gateway
+echo "- List files graphql"
+ls deployment/docker/sales-gateway/graphql
 docker build  -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest deployment/docker/
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
 docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG
