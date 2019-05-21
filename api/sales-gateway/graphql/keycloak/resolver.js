5fd20e6e2783a83a30abc8ad77c95fa11b2697ef
juansantane <juan.santa@nebulae.com.co>
2019-05-21 17:30:19 -0500

    username and password are the parameters required to create and refresh token

--- api/sales-gateway/graphql/keycloak/resolver.js
+++ api/sales-gateway/graphql/keycloak/resolver.js
@@ -1,15 +1,16 @@
 const { of, Observable, bindNodeCallback } = require("rxjs");
 const { map, tap } = require("rxjs/operators");
 const request = require("request");
+const KEYCLOAK_BASE_URL = process.env.KEYCLOAK_BASE_URL;
+const KEYCLOAK_CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID;
 
 module.exports = {
   Mutation: {
     createToken: (root, args, context, info) => {
-      //console.log(JSON.stringify(args));
       return bindNodeCallback(request.post)({
         headers: { "content-type": "application/json" },
-        url: args.base_url,
-        form: args
+        url: KEYCLOAK_BASE_URL,
+        form: {...args, grant_type: 'password', client_id: KEYCLOAK_CLIENT_ID }
       })
         .pipe(map(([resp, jsonStr]) => JSON.parse(jsonStr)))
         .toPromise();
@@ -17,8 +18,8 @@ module.exports = {
     refreshToken: (root, args, context, info) => {
       return bindNodeCallback(request.post)({
         headers: { "content-type": "application/json" },
-        url: args.base_url,
-        form: args
+        url: KEYCLOAK_BASE_URL,
+        form: {...args, grant_type: 'refresh_token', client_id: KEYCLOAK_CLIENT_ID }
       })
         .pipe(map(([resp, jsonStr]) => JSON.parse(jsonStr)))
         .toPromise();
